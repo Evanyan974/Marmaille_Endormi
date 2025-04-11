494d1373161238a5fdf6d69572889ccde6df8fee
Genesis123456789 <e.lubranodiscampa@rubika-edu.re>
2025-04-11 12:38:25 +0400

    Essaie de faire attaquer les ennemis
    Modififications ScriptsEnnemis et Dragons

--- Assets/Projet/Script/Ennemies/Ennemies.cs
+++ Assets/Projet/Script/Ennemies/Ennemies.cs
@@ -7,10 +7,23 @@ public abstract class Ennemies : MonoBehaviour
     private int health;
     private float detectionRange;
     private float range;
+    private float speed;
+    private Transform player;
 
     public int Health { get => health; set => health = value; }
     public float DetectionRange { get => detectionRange; set => detectionRange = value; }
     public float Range { get => range; set => range = value; }
+
+    public float Speed {get => speed; set => speed = value; }
+
+    public Transform Player { get => player; set => player = value; }
+
+
+    public void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
 
     public void Patrol()
     {
@@ -20,9 +33,12 @@ public abstract class Ennemies : MonoBehaviour
     public void Hit()
     {
         
+
+
     }
 
     public abstract void Attack(float detectionRange, float range);
+    
 
     public void DropPower()
     {
